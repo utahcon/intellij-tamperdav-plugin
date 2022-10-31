b43cb7366db9b06dbf39125e0e9f8c9e3490b4b2
GitHub Action <action@github.com>
2022-10-31 20:50:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/utahcon/intellijtamperdavplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/utahcon/intellijtamperdavplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.utahcon.intellijtamperdavplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.utahcon.intellijtamperdavplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
